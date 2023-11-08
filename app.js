ee528fb0f83f512f6feee64975aab0777cbfc079
fabio <github@sauna.re>
2023-11-08 15:49:55 +0100

    Fix debugging warnings

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
